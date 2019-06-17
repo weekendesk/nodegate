76ab9875e9b1c7e0a5e5579d634b35797e703a21
Julien Martin <martin.julien82@gmail.com>
2019-06-17 16:44:36 +0200

    Project modifier added

---
+++ modifiers/project.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) Weekendesk SAS.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const { cloneDeep, get, set } = require('lodash');
+
+module.exports = projections => (container) => {
+  const body = {};
+  projections.forEach((projection) => {
+    set(body, projection[1], get(container.body, projection[0]));
+  });
+  return {
+    ...cloneDeep(container),
+    body,
+  };
+};
---
+++ test/modifiers/project.test.js
@@ -0,0 +1,59 @@
+const project = require('../../modifiers/project');
+
+describe('modifiers/project', () => {
+  it('should correctly return a function', () => {
+    expect(project()).toBeInstanceOf(Function);
+  });
+  it('should project simple values', () => {
+    const result = project([
+      ['ship', 'ship'],
+      ['captain', 'captain'],
+    ])({
+      body: {
+        ship: 'Enterprise',
+        captain: 'Jean-Luc Picard',
+      },
+    });
+    expect(result.body.ship).toEqual('Enterprise');
+    expect(result.body.captain).toEqual('Jean-Luc Picard');
+  });
+  it('should project values into arrays', () => {
+    const result = project([
+      ['ship', 'ships[0]'],
+      ['captain', 'captains[0]'],
+    ])({
+      body: {
+        ship: 'Enterprise',
+        captain: 'Jean-Luc Picard',
+      },
+    });
+    expect(result.body.ships[0]).toEqual('Enterprise');
+    expect(result.body.captains[0]).toEqual('Jean-Luc Picard');
+  });
+  it('should allow to rename values', () => {
+    const result = project([
+      ['ship', 'shipName'],
+      ['captain', 'captainName'],
+    ])({
+      body: {
+        ship: 'Enterprise',
+        captain: 'Jean-Luc Picard',
+      },
+    });
+    expect(result.body.shipName).toEqual('Enterprise');
+    expect(result.body.captainName).toEqual('Jean-Luc Picard');
+  });
+  it('should ignore non wanted values', () => {
+    const result = project([
+      ['ship', 'shipName'],
+      ['captain', 'captainName'],
+    ])({
+      body: {
+        ship: 'Enterprise',
+        captain: 'Jean-Luc Picard',
+        quadran: 'Alpha',
+      },
+    });
+    expect(result.body.quadran).toBeUndefined();
+  });
+});
