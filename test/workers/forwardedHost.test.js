e734338f4ca98774674d8bf3a3e4719abd50ed5b
Julien Martin <martin.julien82@gmail.com>
2019-10-10 11:21:36 +0200

    ForwardedHost worker now mutable

--- test/workers/forwardedHost.test.js
+++ test/workers/forwardedHost.test.js
@@ -5,22 +5,25 @@ describe('workers/forwardedHost', () => {
     expect(forwardedHost()).toBeInstanceOf(Function);
   });
   it('should set the X-Forwarded-Host header to the container', () => {
-    const container = forwardedHost()(
-      { headers: {} },
+    const container = { headers: {} };
+    forwardedHost()(
+      container,
       { headers: { host: 'https://wiki.federation.com' } },
     );
     expect(container.headers['X-Forwarded-Host']).toEqual('https://wiki.federation.com');
   });
   it('should not set the host header', () => {
-    const container = forwardedHost()(
-      { headers: {} },
+    const container = { headers: {} };
+    forwardedHost()(
+      container,
       { headers: { host: 'https://wiki.federation.com' } },
     );
     expect(container.headers.host).toBeFalsy();
   });
   it('should not reset container headers', () => {
-    const container = forwardedHost()(
-      { headers: { authentication: 'Picard, authorization Alpha-Alpha-3-0-5' } },
+    const container = { headers: { authentication: 'Picard, authorization Alpha-Alpha-3-0-5' } };
+    forwardedHost()(
+      container,
       { headers: { host: 'https://wiki.federation.com' } },
     );
     expect(container.headers['X-Forwarded-Host']).toEqual('https://wiki.federation.com');
