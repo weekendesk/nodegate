b89b041928ce45213d326b2a5232012cf973cc2e
Julien Martin <martin.julien82@gmail.com>
2019-10-20 16:12:37 +0200

    Request fully reworked

--- services/request.js
+++ services/request.js
@@ -5,43 +5,63 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+const { get } = require('lodash');
 const requestNative = require('request-promise-native');
-const { getConfiguration } = require('./configuration');
+const { getConfiguration } = require('../services/configuration');
 
-const defaultOptions = () => getConfiguration().request;
-
-const getHeaders = (container, options) => ({
-  ...((defaultOptions() && defaultOptions().headers) || {}),
-  ...((container && container.headers) || {}),
-  ...((options && options.headers) || {}),
-});
-
-const doRequest = (container, method, url, options) => {
-  const parameters = {
-    ...defaultOptions(),
-    ...options,
-    headers: getHeaders(container, options),
-    method,
-    url: (typeof url === 'function' && url(container)) || url,
-    body: (container && container.body) || {},
-    qs: (container && container.query) || {},
-  };
-
-  return requestNative(parameters);
+const project = (container, value) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    Object.keys(value).forEach((key) => {
+      value[key] = project(container, value[key]);
+    });
+    return value;
+  }
+  if (typeof value === 'string') {
+    return get(container, value);
+  }
+  throw new TypeError('Invalid projection, must be an object or a string');
 };
 
-const request = (container) => ({
-  get: (url, options) => doRequest(container, 'get', url, options),
-  post: (url, options) => doRequest(container, 'post', url, options),
-  patch: (url, options) => doRequest(container, 'patch', url, options),
-  put: (url, options) => doRequest(container, 'put', url, options),
-  delete: (url, options) => doRequest(container, 'delete', url, options),
-});
+const validateArguments = (container, method, url, options) => {
+  if (typeof container !== 'object' || Array.isArray(container)) {
+    throw new TypeError('Invalid argument: "container", must be an object');
+  }
+  if (typeof method !== 'string') {
+    throw new TypeError('Invalid argument: "method", must be a string');
+  }
+  if (typeof url !== 'string' && typeof url !== 'function') {
+    throw new TypeError('Invalid argument: "url", must be an string or a function');
+  }
+  if (typeof options !== 'object' || Array.isArray(options)) {
+    throw new TypeError('Invalid argument: "options", must be an object');
+  }
+};
 
-request.get = (url, options) => doRequest(null, 'get', url, options);
-request.post = (url, options) => doRequest(null, 'post', url, options);
-request.patch = (url, options) => doRequest(null, 'patch', url, options);
-request.put = (url, options) => doRequest(null, 'put', url, options);
-request.delete = (url, options) => doRequest(null, 'delete', url, options);
+const projectKey = (key, requestOptions, container, options) => {
+  if (options[key] === null) {
+    requestOptions[key] = null;
+  } else if (!options[key]) {
+    requestOptions[key] = container[key] || null;
+  } else {
+    requestOptions[key] = project(container, options[key]);
+  }
+};
+
+const request = (container, method, url, options = {}) => {
+  validateArguments(container, method, url, options);
+
+  const { request: configuration } = getConfiguration();
+  const requestOptions = {
+    ...configuration,
+    ...options,
+    method,
+    uri: typeof url === 'function' ? url(container) : url,
+  };
+
+  projectKey('headers', requestOptions, container, options);
+  projectKey('body', requestOptions, container, options);
+
+  return requestNative(requestOptions);
+};
 
 module.exports = request;
